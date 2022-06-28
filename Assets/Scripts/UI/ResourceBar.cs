61577ac1a796a9f4e9bd2d2616e22fd6f711195d
Jace <35653547+Cyno00@users.noreply.github.com>
2022-06-28 17:27:25 -0400

    TextMeshPro Import and Resource Bar Text

--- Assets/Scripts/UI/ResourceBar.cs
+++ Assets/Scripts/UI/ResourceBar.cs
@@ -2,25 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ResourceBar : MonoBehaviour
 {
     public Slider slider;
     public Image fill;
+    public ResourceBarTextField currentText;
+    public ResourceBarTextField dividerText;
+    public ResourceBarTextField maxText;
+
+
+    public void Init(resource type, float max, float current)
+    {
+        fill.color = UITheme.resourceColor[type];
+        slider.maxValue = max;
+        slider.value = current;
+        UpdateText();
+    }
 
     public void SetType(resource type)
     {
         fill.color = UITheme.resourceColor[type];
+        UpdateText();
     }
 
     public void SetMax(float max)
     {
         slider.maxValue = max;
-        slider.value = max;
+        UpdateText();
     }
 
-    public void SetFill(float resourceLevel)
+    public void SetFill(float current)
     {
-        slider.value = resourceLevel;
+        slider.value = current;
+        UpdateText();
+    }
+
+    public void UpdateText()
+    {
+        currentText.text.text = (slider.value).ToString();
+        maxText.text.text = (slider.maxValue).ToString();
     }
 }
