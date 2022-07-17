15b65781f349722fda776bc21de95f42746d0d01
zfxd <zhang_weiyao@yahoo.com.sg>
2022-07-16 21:13:27 -0700

    Not much
    Slight faffing about, thinking about how to make a skill

---
+++ Assets/Scripts/Skills/basic.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Combat;
+
+/* Basic, single target attack.
+*/
+public static class BasicSingle
+{
+    // Skill info
+    TargetMode mode = SINGLE;
+
+    // Do stuff
+}
